74e8b05e8c90e9dc6dab4d7fae73ee1dd38b02bb
Wasswa Samuel <latimerscope@gmail.com>
2020-06-15 15:31:45 +0200

    Add Categories to Videos at DB Level

--- lib/rumbl/multimedia/video.ex
+++ lib/rumbl/multimedia/video.ex
@@ -7,13 +7,14 @@ defmodule Rumbl.Multimedia.Video do
     field :title, :string
     field :url, :string
     belongs_to :user, Rumbl.Accounts.User
+    belongs_to :category, Rumbl.Multimedia.Category
     timestamps()
   end
 
   @doc false
   def changeset(video, attrs) do
     video
-    |> cast(attrs, [:url, :title, :description])
+    |> cast(attrs, [:url, :title, :description, :category_id])
     |> validate_required([:url, :title, :description])
   end
 end
