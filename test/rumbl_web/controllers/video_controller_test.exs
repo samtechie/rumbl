cffd626c892af3335d1bc46d57968b58ade418db
Wasswa Samuel <latimerscope@gmail.com>
2020-07-09 16:23:26 +0200

    Test Logged-In Users
    Add testing for logged in users and using tags. Also modified the Auth plug to make it more testable

--- test/rumbl_web/controllers/video_controller_test.exs
+++ test/rumbl_web/controllers/video_controller_test.exs
@@ -18,4 +18,28 @@ defmodule RumblWeb.VideoControllerTest do
       end
     )
   end
+
+  describe "with a logged-in user" do
+    setup %{conn: conn, login_as: username} do
+      user = user_fixture(username: username)
+      conn = assign(conn, :current_user, user)
+      {:ok, conn: conn, user: user}
+    end
+
+    @tag login_as: "max"
+    test "lists all user's videos on index", %{conn: conn, user: user} do
+      user_video = video_fixture(user, title: "funny cats")
+
+      other_video =
+        video_fixture(
+          user_fixture(username: "other"),
+          title: "another video"
+        )
+
+      conn = get(conn, Routes.video_path(conn, :index))
+      assert html_response(conn, 200) =~ ~r/Listing Videos/
+      assert String.contains?(conn.resp_body, user_video.title)
+      refute String.contains?(conn.resp_body, other_video.title)
+    end
+  end
 end
