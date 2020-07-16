f39424b365a52d175248716b9378a5348be183e0
Wasswa Samuel <latimerscope@gmail.com>
2020-07-16 11:52:02 +0200

    Add Javascript for watching youtube videos

--- assets/js/app.js
+++ assets/js/app.js
@@ -13,3 +13,11 @@ import "../css/app.scss"
 //     import socket from "./socket"
 //
 import "phoenix_html"
+
+import Player from "./player"
+let video = document.getElementById("video")
+if (video) {
+  Player.init(video.id, video.getAttribute("data-player-id"), () => {
+    console.log("player ready!")
+  })
+}
