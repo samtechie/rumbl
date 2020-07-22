487c7fcd00532120b589fc401cff59dd4076f512
Wasswa Samuel <latimerscope@gmail.com>
2020-07-22 15:40:09 +0200

    create and add channel

--- lib/rumbl_web/channels/user_socket.ex
+++ lib/rumbl_web/channels/user_socket.ex
@@ -4,6 +4,8 @@ defmodule RumblWeb.UserSocket do
   ## Channels
   # channel "room:*", RumblWeb.RoomChannel
 
+  channel "videos:*", RumblWeb.VideoChannel
+
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
   # verification, you can put default assigns into
