7e56106cad020250916a661f5d8c75e7e4c491b1
Wasswa Samuel <latimerscope@gmail.com>
2020-07-28 22:04:52 +0200

    Join channel from client

--- assets/js/video.js
+++ assets/js/video.js
@@ -19,6 +19,10 @@ let Video = {
     let vidChannel = socket.channel("videos:" + videoId)
     // TODO join the vidChannel
 
+    vidChannel.join()
+    receive("ok", resp => console.log("joined the video channel", resp))
+    receive("error", reason => console.log("join failed", reason))
+
   }
 }
 export default Video
